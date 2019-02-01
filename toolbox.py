18bb6783ed53e039c5e51f55163e156743f0b41f
Isaac Avram <avrisaac555@gmail.com>
2019-01-31 21:37:33 -0800

    Added error message for failed lookups

--- toolbox.py
+++ toolbox.py
@@ -3,6 +3,8 @@ import argparse
 import cmds
 import sys
 
+PROG_NAME = "toolbox.py"
+PROG_VERSION = "0.1.0"
 _commands = cmds.load_commands()
 
 def run_command(name: str, args: List[str], **kvargs):
@@ -16,11 +18,17 @@ def run_command(name: str, args: List[str], **kvargs):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description=f"Executes a command included in this package. Available commands: {list(_commands.values())}"
-        )
+        prog=PROG_NAME,
+        description=f"Executes a command included in this package. Available commands: {list(_commands.values())}",
+        epilog=f"Version: {PROG_VERSION}")
     parser.add_argument("cmd_name", metavar="CMD", type=str, help="The name of the command to run.")
     parser.add_argument("cmd_args", metavar="ARG", type=str, nargs="*", help="Arguments to pass to the command.")
 
     args = parser.parse_args()
-    exit_code = run_command(args.cmd_name, args.cmd_args)
-    sys.exit(exit_code)
+
+    try:
+        exit_code = run_command(args.cmd_name, args.cmd_args)
+        sys.exit(exit_code)
+    except KeyError as e:
+        print(f"{PROG_NAME}: error: Invalid command: {e}", file=sys.stderr)
+        sys.exit(1)
