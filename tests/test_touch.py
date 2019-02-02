b4ec68d9f5fbb60fc8d6293c0d8929950f162599
Isaac Avram <avrisaac555@gmail.com>
2019-02-02 10:46:18 -0800

    Fixed stray parenthesis

--- tests/test_touch.py
+++ tests/test_touch.py
@@ -11,7 +11,7 @@ current_year = datetime.date.today().year
     ("9702021234.15",   datetime.datetime(1997, 2, 2, 12, 34, 15)),
     ("1401010730",      datetime.datetime(2014, 1, 1, 7, 30)),
     ("1501020420.20",   datetime.datetime(2015, 1, 2, 4, 20, 20)),
-    ("240103211234"),   datetime.datetime(2401, 3, 21, 12, 34),
+    ("240103211234",    datetime.datetime(2401, 3, 21, 12, 34)),
     ("240103211234.20", datetime.datetime(2401, 3, 21, 12, 34, 20))
 ])
 def test_unix_time_decimal(func_input, expected):
