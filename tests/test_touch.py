3ee53852ca91e995b4535be0e723b9b23cf3ba2f
Isaac Avram <avrisaac555@gmail.com>
2019-02-02 10:40:00 -0800

    Added gitignore and __init__ for tests

--- tests/test_touch.py
+++ tests/test_touch.py
@@ -4,7 +4,7 @@ import pytest
 
 current_year = datetime.date.today().year
 
-@pytest.mark.parametrize("func_input, expected", [
+@pytest.mark.parametrize(("func_input", "expected"), [
     ("04200420",        datetime.datetime(current_year, 4, 20, 4, 20)),
     ("10111213.14",     datetime.datetime(current_year, 10, 11, 12, 13, 14)),
     ("9709091234",      datetime.datetime(1997, 9, 9, 12, 34)),
