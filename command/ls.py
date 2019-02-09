4c3960a5aa9b4e9917b06ff28e4df7118680c3bc
Isaac Avram <avrisaac555@gmail.com>
2019-02-08 19:30:35 -0800

    I forgot to commit

--- command/ls.py
+++ command/ls.py
@@ -1,6 +1,20 @@
 from typing import List
 from enum import Enum, Flag, auto
 import argparse
+import re
+
+
+class EnumArgParseMixin():
+    camel_match = re.compile(r"(.)([A-Z][a-z])")
+
+    @classmethod
+    def add_arguments(cls, parser, args: List[tuple], dest=None):
+        if not dest:
+            dest = camel_match.sub("\g<1>_\g<2>", dest).lower()
+
+        for flag, const, pass_through in args:
+            parser.add_argument(flag, action="store_const", dest=dest, const=const, **pass_through)
+
 
 class LongOutputMethod(Flag):
     SHORT = 0
@@ -14,29 +28,17 @@ class LongOutputMethod(Flag):
         return parser.add_argument(name, action=flag_or_action(cls), dest=dest, const=const, **kwargs)
 
 
-class EntryOutput(Enum):
+class EntryOutput(EnumArgParseMixin, Enum):
     REGULAR = 0
     SHOW_HIDDEN = 1
     SHOW_HIDDEN_PLUS = 2
 
-    @classmethod
-    def add_argument(cls, parser, args: List[dict], dest="entry_output")
-        for a in args:
-            if 'flag' not in a:
-                raise ValueError("Entries in args must contain a key 'flag'")
-            
-            if 'const' not in a:
-                raise ValueError("Entries in args must contain a key 'const")
 
-            parser.add_argument(a.pop('flag'), action="store_const", dest=dest, **a)
-
-
-class ShortOutputFormat(Enum):
-    COLUMNS= 0
+class ShortOutputFormat(EnumArgParseMixin, Enum):
+    COLUMNS = 0
     ROWS = 1
     STREAM = 2
     LIST = 3
-
 
 
 def flag_or_action(flag_type):
@@ -47,15 +49,17 @@ def flag_or_action(flag_type):
     If a dest already exists in the namespace, then dest will be ORed with itself and const. Otherwise it is set to const.
     If no const is specified, flag_type(0) is used.
     """
-    def action_constructor(option_string, dest, **kwargs):
-        def perform_action(parser, namespace, values, option_string = None):
+    def action_constructor(dest, **kwargs):
+        def perform_action(parser, namespace, values, option_string=None):
             if hasattr(namespace, dest):
                 setattr(namespace, dest, getattr(namespace, dest) | kwargs.get('const', flag_type(0)))
             else:
                 setattr(namespace, dest, kwargs.get('const', flag_type(0)))
-    
+
         return perform_action
+    
     return action_constructor
+
 
 arg_parser = argparse.ArgumentParser(
     prog="ls",
@@ -75,7 +79,11 @@ LongOutputMethod.add_argument(arg_parser, "-o", LongOutputMethod.LONG | LongOutp
 EntryOutput.add_argument(
     arg_parser.add_mutually_exclusive_group(),
     [
-        {"flag": "-a", "const": EntryOutput.SHOW_HIDDEN, help="Write out all directory entries. Including those starting with '.'. If on Windows, this will also list files marked as hidden."},
-        {"flag": "-A", "const": EntryOutput.SHOW_HIDDEN_PLUS, help="Same as -a but also lists '.' and '..'. "}
+        ("-a", EntryOutput.SHOW_HIDDEN, {"help": "Write out all directory entries. Including those starting with '.'. If on Windows, this will also list files marked as hidden."}),
+        ("-A", EntryOutput.SHOW_HIDDEN_PLUS, {"help": "Same as -a but also lists '.' and '..'. "})
     ]
 )
+
+
+def _cmd_main(args: List[str]):
+    parsed_args = arg_parser.parse_args(args)
