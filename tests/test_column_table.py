fd8e1471a0860f64a7273dfcd549b9b34973afc4
Isaac Avram <avrisaac555@gmail.com>
2019-02-14 17:08:10 -0800

    Added unit tests for compress_columns

--- tests/test_column_table.py
+++ tests/test_column_table.py
@@ -7,8 +7,22 @@ import pytest
     (Table([["a"], ["b"], ["c"], ["d"]]), 3, 1, ["b", "c", "d"], [["a"]]),
     (Table([["a", "b"], ["c", "d"], ["e"], ["f"]]), 1, 1, ["c"], [["a", "b"], ["d"], ["e"], ["f"]]),
     (Table([["a", "b"], ["c", "d"], ["e"], ["f"]]), 3, 1, ["c", "d", "e"], [["a", "b"], ["f"]]),
+    (Table([["a"]]), 1, 0, ["a"], [[]])
 ])
 def test_take(table, n, start, expected_rv, expected_state):
     actual = table._take(n, start)
     assert actual == expected_rv
+    assert table.columns == expected_state
+
+
+@pytest.mark.parametrize("table,r,expected_state", [
+    (Table([["a"], ["b"], ["c"], ["d"]]), 2, [["a", "b"], ["c", "d"]]),
+    (Table([["a"], ["b"], ["c"], ["d"]]), 3, [["a", "b", "c"], ["d"]]),
+    (Table([["a"], ["b"], ["c"], ["d"]]), 2, [["a", "b"], ["c", "d"]]),
+    (Table([["a", "b", "c"], ["d", "e"], ["f"]]), 3, [["a", "b", "c"], ["d", "e", "f"]]),
+    (Table([["a"], ["b", "c"], ["d", "e", "f"]]), 3, [["a", "b", "c"], ["d", "e", "f"]]),
+])
+def test_compress_columns(table, r, expected_state):
+    table._compress_columns(r)
+
     assert table.columns == expected_state
